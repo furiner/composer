d2f7c8673dc17d533279a4f058cb0f83790ecd0b
Irisu <nekomarisakirisame@gmail.com>
2023-02-01 14:56:06 -0500

    :3

---
+++ composer.Desktop/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using composer.Editor;
+using osu.Framework;
+using osu.Framework.Platform;
+
+namespace composer.Desktop
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            using (DesktopGameHost host = Host.GetSuitableDesktopHost("owo"))
+            using (var game = new EditorGame())
+            {
+                host.Run(game);
+            }
+        }
+    }
+}
---
+++ composer.Editor/EditorGame.cs
@@ -0,0 +1,19 @@
+﻿using osu.Framework.Allocation;
+using osu.Game;
+
+namespace composer.Editor
+{
+    public class EditorGame : OsuGameBase
+    {
+        [BackgroundDependencyLoader]
+        private void load()
+        {
+            
+        }
+        
+        protected override void LoadComplete()
+        {
+            base.LoadComplete();
+        }
+    }
+}
