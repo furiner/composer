f38c2ab5fd369f91e085c792318eff6c69943523
Irisu <nekomarisakirisame@gmail.com>
2023-02-03 02:08:17 -0500

    add plugin store

---
+++ composer.Editor/Plugins/PluginStore.cs
@@ -0,0 +1,19 @@
+﻿using osu.Framework.Platform;
+
+namespace composer.Editor.Plugins
+{
+    public abstract class PluginStore : IDisposable
+    {
+        private Storage storage;
+        
+        protected PluginStore(Storage storage)
+        {
+            this.storage = storage;
+        }
+
+        public void Dispose()
+        {
+            
+        }
+    }
+}
