2acce5bd1aecd26b404f47d1514ea410f894f10c
Irisu <nekomarisakirisame@gmail.com>
2023-02-05 08:01:46 -0500

    add a basic rpc to the desktop instance

---
+++ composer.Desktop/Online/DiscordRichPresence.cs
@@ -0,0 +1,30 @@
+﻿using DiscordRPC;
+using DiscordRPC.Message;
+using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+
+namespace composer.Desktop.Online
+{
+    public partial class DiscordRichPresence : Component
+    {
+        private const string application_id = "1071771476409192448";
+        private DiscordRpcClient client = null!;
+
+        public readonly RichPresence CurrentPresence = new RichPresence()
+            { };
+
+        [BackgroundDependencyLoader]
+        private void load()
+        {
+            client = new DiscordRpcClient(application_id);
+
+            client.OnReady += onReady;
+
+            client.Initialize();
+        }
+        
+        private void onReady(object sender, ReadyMessage readyEvent) {
+            
+        }
+    }
+}
