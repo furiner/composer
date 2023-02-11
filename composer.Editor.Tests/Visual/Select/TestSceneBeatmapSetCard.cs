0c41732a093d852eb89329a6eb1ec6c0f01afb08
naoei <altenyt@gmail.com>
2023-02-11 17:55:33 -0500

    Implement beatmap set card design

---
+++ composer.Editor.Tests/Visual/Select/TestSceneBeatmapSetCard.cs
@@ -0,0 +1,28 @@
+﻿using composer.Editor.Screens.Select.Carousel;
+using composer.Editor.Tests.Resources;
+using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Game.Rulesets.Catch;
+using osu.Game.Rulesets.Mania;
+using osu.Game.Rulesets.Osu;
+using osu.Game.Rulesets.Taiko;
+using osu.Game.Tests.Visual;
+using osuTK;
+
+namespace composer.Editor.Tests.Visual.Select
+{
+    public partial class TestSceneBeatmapSetCard : OsuTestScene
+    {
+        [BackgroundDependencyLoader]
+        private void load()
+        {
+            Add(new BeatmapSetCard(TestResources.CreateTestBeatmapSetInfo(null,
+                new[] { new OsuRuleset().RulesetInfo, new TaikoRuleset().RulesetInfo, new CatchRuleset().RulesetInfo, new ManiaRuleset().RulesetInfo }))
+            {
+                Size = new Vector2(720, 80),
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre
+            });
+        }
+    }
+}
---
+++ composer.Editor/Screens/Select/Carousel/SetPanelContent.cs
@@ -0,0 +1,113 @@
+﻿using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Shapes;
+using osu.Framework.Localisation;
+using osu.Game.Beatmaps;
+using osu.Game.Graphics;
+using osu.Game.Graphics.Sprites;
+using osuTK;
+
+namespace composer.Editor.Screens.Select.Carousel
+{
+    public partial class SetPanelContent : CompositeDrawable
+    {
+        private readonly BeatmapSetInfo beatmapSet;
+
+        private FillFlowContainer iconFlow = null!;
+
+        public SetPanelContent(BeatmapSetInfo beatmapSet)
+        {
+            this.beatmapSet = beatmapSet;
+
+            RelativeSizeAxes = Axes.Both;
+        }
+
+        [Resolved]
+        private OsuColour colours { get; set; } = null!;
+
+        [BackgroundDependencyLoader]
+        private void load()
+        {
+            InternalChild = new FillFlowContainer
+            {
+                RelativeSizeAxes = Axes.Both,
+                Direction = FillDirection.Vertical,
+                Padding = new MarginPadding(12),
+                Children = new Drawable[]
+                {
+                    new OsuSpriteText
+                    {
+                        Text = new RomanisableString(beatmapSet.Metadata.TitleUnicode, beatmapSet.Metadata.Title),
+                        Font = OsuFont.GetFont(Typeface.Inter, weight: FontWeight.SemiBold, size: 16),
+                        Shadow = true
+                    },
+                    new OsuSpriteText
+                    {
+                        Text = new RomanisableString(beatmapSet.Metadata.ArtistUnicode, beatmapSet.Metadata.Artist),
+                        Font = OsuFont.GetFont(Typeface.Inter, weight: FontWeight.Regular, size: 16),
+                        Shadow = true,
+                        Alpha = 0.5f
+                    },
+                    new FillFlowContainer
+                    {
+                        Direction = FillDirection.Horizontal,
+                        AutoSizeAxes = Axes.Both,
+                        Margin = new MarginPadding { Top = 8 },
+                        Child = iconFlow = new FillFlowContainer
+                        {
+                            AutoSizeAxes = Axes.Both,
+                            Origin = Anchor.CentreLeft,
+                            Anchor = Anchor.CentreLeft,
+                            Spacing = new Vector2(8),
+                        }
+                    }
+                }
+            };
+        }
+
+        protected override void LoadComplete()
+        {
+            base.LoadComplete();
+            iconFlow.ChildrenEnumerable = getDifficultyIcons();
+        }
+        
+        private const int maximum_difficulty_lines = 18;
+
+        private IEnumerable<Drawable> getDifficultyIcons()
+        {
+            var beatmaps = beatmapSet.Beatmaps.ToList();
+            var rulesetGrouped = beatmaps.GroupBy(b => b.Ruleset);
+            
+            foreach (var info in rulesetGrouped)
+            {
+                var flowContainer = new FillFlowContainer
+                {
+                    Direction = FillDirection.Horizontal,
+                    AutoSizeAxes = Axes.Both,
+                    Origin = Anchor.CentreLeft,
+                    Anchor = Anchor.CentreLeft,
+                    Spacing = new Vector2(2)
+                };
+                
+                flowContainer.Add(info.Key.CreateInstance().CreateIcon().With(d => d.Size = new Vector2(16)));
+                var groupedBeatmaps = info.ToList();
+                groupedBeatmaps.Sort((b1, b2) => (int) (b1.StarRating - b2.StarRating));
+                
+                foreach (var beatmap in groupedBeatmaps)
+                {
+                    flowContainer.Add(new Circle
+                    {
+                        RelativeSizeAxes = Axes.Y,
+                        Width = 4,
+                        Origin = Anchor.CentreLeft,
+                        Anchor = Anchor.CentreLeft,
+                        Colour = colours.ForStarDifficulty(beatmap.StarRating)
+                    });
+                }
+                
+                yield return flowContainer;
+            }
+        }
+    }
+}
